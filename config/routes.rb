81ee901c17e30e640ee87ee25f06ac3523b4767b
0gravity <0gravity000@gmail.com>
2021-08-05 07:03:48 +0000

    Add login new/create logout router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   get '/signup',    to: 'users#new'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
