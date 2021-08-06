51a81fc547ba157e23c80a6a78989e3a7fa2ddd7
0gravity <0gravity000@gmail.com>
2021-08-06 09:27:56 +0000

    Add Send email test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
+  config.action_mailer.default_url_options = { host: '4b558c6bd78c471da045c79078721616.vfs.cloud9.ap-northeast-1.amazonaws.com' } #自分の環境に合わせてください。
+  
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
